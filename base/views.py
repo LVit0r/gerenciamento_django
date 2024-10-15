927e0282963bdf340b10e6eaf4fadc4822138340
luiz <luizvitoralves@gmail.com>
2024-10-15 09:37:36 -0300

    mudando login

--- base/views.py
+++ base/views.py
@@ -3,4 +3,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    return render(request, 'usaveis/base.html' )
+    return render(request, 'cont/index.html' )
